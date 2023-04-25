c9543611e56bc764e1261043081d4944900da664
planner-sohrab <plannersohrab@gmail.com>
2023-04-25 12:38:10 +0600

    Build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
 import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div>
-      <Grid
-        templateAreas={{
-          base: `"nav" "main" `,
-          lg: `"nav nav" "aside main" `,
-        }}
-      >
-        <GridItem area="nav">
-          <Navbar />
-        </GridItem>
-        <Show above="lg">
-          <GridItem area="aside" bg="gold">
-            aside
-          </GridItem>
-        </Show>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main" `,
+        lg: `"nav nav" "aside main" `,
+      }}
+    >
+      <GridItem area="nav">
+        <Navbar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside">aside</GridItem>
+      </Show>
 
-        <GridItem area="main" bg="coral">
-          Main
-        </GridItem>
-      </Grid>
-    </div>
+      <GridItem area="main">Main</GridItem>
+    </Grid>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/react.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const Navbar = () => {
   return (
-    <div>
-      <HStack>
-        <Image src={logo} boxSize="60px" />
-      </HStack>
-    </div>
+    <HStack justifyContent="space-between" padding="10px">
+      <Image src={logo} boxSize="60px" />
+
+      <ColorModeSwitch />
+    </HStack>
   );
 };
 
